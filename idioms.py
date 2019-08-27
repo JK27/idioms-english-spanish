602b9d403b7af598104d5866b214547dd610afa1
Ubuntu <ubuntu@ip-172-31-95-175.ec2.internal>
2019-08-27 15:29:13 +0000

    Created base.html and idioms.html

--- idioms.py
+++ idioms.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "idioms"
+app.config["MONGO_URI"] = "mongodb+srv://jk27:07diciembre18@cluster0-9pga0.mongodb.net/idioms-and-proverbs?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/get_idioms")
+def get_idioms():
+    return render_template("idioms.html", idioms=mongo.db.idioms.find())
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
