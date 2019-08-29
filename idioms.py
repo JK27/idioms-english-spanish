51753367b3547932a1fed7d7dc17234029969748
Ubuntu <ubuntu@ip-172-31-95-175.ec2.internal>
2019-08-29 13:30:40 +0000

    Added editidiom.html and functionality to edit and update idioms

--- idioms.py
+++ idioms.py
@@ -14,15 +14,36 @@ mongo = PyMongo(app)
 @app.route("/get_idioms")
 def get_idioms():
     return render_template("idioms.html", idioms=mongo.db.idioms.find())
-    
+
+# ---------------------------------------------------------------------     Functions to add idioms -- #
 @app.route("/add_idiom")
 def add_idiom():
     return render_template("addidiom.html")
     
-@app.route("/insert_idiom", methods=["POST"])
+@app.route("/insert_idiom", methods=["POST"])                           #-- Data from form in addidiom.html is posted using this function...
 def insert_idiom():
+    idioms = mongo.db.idioms                                            #-- ... data is inserted to the idioms database...
+    idioms.insert_one(request.form.to_dict())                           #-- ... inserting one item to the dictionary...
+    return redirect(url_for("get_idioms"))                              #-- ... and then redirecting to /get_idioms
+    
+# ---------------------------------------------------------------------     Functions to edit idioms -- #    
+@app.route("/edit_idiom/ <idiom_id>")
+def edit_idiom(idiom_id):                                               #-- Use idiom_id to target the idiom that want to edit
+    the_idiom = mongo.db.idioms.find_one({"_id":ObjectId(idiom_id)})    #-- Retrieve the idiom from the idioms database, using its id to find it...
+    return render_template("editidiom.html", idiom=the_idiom)           #-- ... and then redirecting to /editidiom.html
+    
+@app.route("/update_idiom/<idiom_id>", methods=["POST"])
+def update_idiom(idiom_id):
     idioms = mongo.db.idioms
-    idioms.insert_one(request.form.to_dict())
+    idioms.update({"_id":ObjectId(idiom_id)},
+    {
+        "spanish_idiom": request.form.get("spanish_idiom"),
+        "english_literal": request.form.get("english_literal"),
+        "english_meaning": request.form.get("english_meaning"),
+        "english_idiom": request.form.get("english_idiom"),
+        "spanish_literal": request.form.get("spanish_literal"),
+        "spanish_meaning": request.form.get("spanish_meaning")
+    })
     return redirect(url_for("get_idioms"))
     
     
