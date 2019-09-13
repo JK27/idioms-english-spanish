bd5fbd9b98d1b854f5cfd1d1e1d7495fceacf386
Ubuntu <ubuntu@ip-172-31-95-175.ec2.internal>
2019-09-13 10:02:43 +0000

    changed pagination method

--- idioms.py
+++ idioms.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
@@ -14,50 +14,77 @@ mongo = PyMongo(app)
 @app.route("/get_idioms")
 def get_idioms():
     
-    pageSize = 10
-    pageNum = 1
+    idiom = mongo.db.idioms
     
-    return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+    offset = 20
+    pageSize = 3
+    
+    starting_id = idiom.find().sort('_id', pymongo.DESCENDING)
+    last_id = starting_id[offset]['_id']
+    
+    idioms = idiom.find({'_id' : {'$lte' : last_id}}).limit(pageSize)
+    
+        
+    next_url = '/idioms?pageSize=' + str(pageSize) + '&offset=' + str(offset + pageSize)
+    prev_url = '/idioms?pageSize=' + str(pageSize) + '&offset=' + str(offset - pageSize)
+    
+    return render_template("idioms.html", idioms=idiom.find({'_id' : {'$lte' : last_id}}).limit(pageSize))
 
-@app.route("/page_two")
-def page_two():
+
     
-    pageSize = 10
-    pageNum = 2
     
-    return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
     
-@app.route("/page_three")
-def page_three():
     
-    pageSize = 10
-    pageNum = 3
+#################################################################################################################
+
+    # return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+
+    # return jsonify({'result' : output, 'prev_url' :prev_url, 'next_url' : next_url})
+
+#     pageSize = 10
+#     pageNum = 1
     
-    return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+#     return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+
+# @app.route("/page_two")
+# def page_two():
     
-@app.route("/page_four")
-def page_four():
+#     pageSize = 10
+#     pageNum = 2
     
-    pageSize = 10
-    pageNum = 4
+#     return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
     
-    return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+# @app.route("/page_three")
+# def page_three():
     
-@app.route("/page_five")
-def page_five():
+#     pageSize = 10
+#     pageNum = 3
     
-    pageSize = 10
-    pageNum = 5
+#     return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
     
-    return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+# @app.route("/page_four")
+# def page_four():
     
-@app.route("/page_six")
-def page_six():
+#     pageSize = 10
+#     pageNum = 4
     
-    pageSize = 10
-    pageNum = 6
+#     return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
     
-    return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+# @app.route("/page_five")
+# def page_five():
+    
+#     pageSize = 10
+#     pageNum = 5
+    
+#     return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
+    
+# @app.route("/page_six")
+# def page_six():
+    
+#     pageSize = 10
+#     pageNum = 6
+    
+#     return render_template("idioms.html", idioms=mongo.db.idioms.find().skip(pageSize*(pageNum-1)).limit(pageSize).sort("_id", -1))
 
 
 
